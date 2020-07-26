eeabe07e00233cae1628e62d74af903b46732b91
steelerx155 <steelerx155@gmail.com>
2020-07-26 15:07:34 -0400

    Done.

--- .history/src/index_20200726144932.js
+++ .history/src/index_20200726144932.js
@@ -3,21 +3,16 @@ import ReactDOM from 'react-dom';
 import { createStore} from 'redux'
 import { provider} from 'react-redux'
 import App from './App'
+
 import manageUsers from './reducers/manageUsers'
 
 
 // add imports and code
-const store = createStore(
-  shoppingListItemReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
 
 
 ReactDOM.render(
   // add imports and code
-  <Provider store = {store}>
   <App />
-  </Provider>
   // add imports and code
   ,
   document.getElementById('root')
--- .history/src/index_20200726145047.js
+++ .history/src/index_20200726145047.js
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom';
 import { createStore} from 'redux'
 import { provider} from 'react-redux'
 import App from './App'
+
 import manageUsers from './reducers/manageUsers'
 
 
 // add imports and code
-const store = createStore(
-  shoppingListItemReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
 
 
 ReactDOM.render(
