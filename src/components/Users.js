abaa775d075b601214bc0e9a55bc9afd1a5d2e33
Amber Taranto <ambzzz@gmail.com>
2020-08-05 15:18:45 -0700

    Done.

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,21 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // add any needed imports here
-class Users extends Component {
+import { connect } from "react-redux";
 
+class Users extends Component {
   render() {
+    console.log(this.props.users);
     return (
       <div>
         <ul>
           Users!
           {/* Write code here that displays the usernames of all users in the Redux store */}
+          {this.props.users.map((user, index) => (
+            <li key={index}> {user.username}</li>
+          ))}
           {/* In addition, display the total number of users curently in the store */}
+          <p>{this.props.userCount}</p>
         </ul>
       </div>
-    )
+    );
   }
 }
 
 //add mapStateToProps here
+const mapStateToProps = (state) => {
+  return {
+    users: state.users,
+    userCount: state.users.length,
+  };
+};
 
 // connect this component to Redux
-export default Users
+export default connect(mapStateToProps)(Users);
--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-import manageUsers from './reducers/manageUsers'
+import manageUsers from "./reducers/manageUsers";
 
+// In src/index.js, use the createStore method from redux, passing in the provided reducer,
+// manageUsers, to create a store.
+
+//Use Provider from react-redux to wrap <App />, passing store as a prop to the Provider. T
+//his will give your components access to the store.
 
 // add imports and code
-
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+const store = createStore(
+  manageUsers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__({ trace: true })
+);
 
 ReactDOM.render(
   // add imports and code
-  <App />
+  <Provider store={store}>
+    <App />
+  </Provider>,
   // add imports and code
-  ,
-  document.getElementById('root')
+  document.getElementById("root")
 );
